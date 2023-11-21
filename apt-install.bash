2a373652cf9ffd29eff40f0bae33eda38a1fc902
Ryazansev Vladimir <elf.forest@yandex.ru>
2023-11-21 22:13:10 +0300

    Вт 21 ноя 2023 22:13:10 MSK

---
+++ apt-install.bash
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+sudo apt install -y build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo
+
+sudo apt install -y gcc-10 libgccjit0 libgccjit-10-dev
+
+sudo apt install -y git mc curl translate-shell buku mpv
+
+# emacs
+sudo apt install sbcl spell aspell aspell-ru graphviz hunspell hunspell-ru sqlite openjdk-19-jre-headless openjdk-19-jdk-headless clangd hunspell-en-us 
